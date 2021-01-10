21f1a558806b608377f53017cab206e44e3c3df9
Melissa <34263527+mbggg@users.noreply.github.com>
2021-01-09 23:36:29 -0500

    importing react and react-dom dependencies

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello Goons!</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
